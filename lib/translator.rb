8c6422e61c952e12090f43191b40b97454646377
Luke Granger <grangerl330@gmail.com>
2019-01-12 02:08:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,11 +6,11 @@ def load_library(file_path)
   get_meaning = {}
   get_emoticon = {}
   library.each do |meaning, emoticon|
-    get_meaning[emoticon[1]] = meaning
-    get_emoticon[emoticon[0]] = emoticon[1]
+    get_meaning[emoticon[1]] = meaning  #emoticon[1] is the second index of the array, which is the japanese emoticons
+    get_emoticon[emoticon[0]] = emoticon[1] #emoticon[0] is the first index of the array, which is the english emoticons
   end 
-  result = {"get_meaning" => get_meaning, "get_emoticon" => get_emoticon}
-  result
+  return_value = {"get_meaning" => get_meaning, "get_emoticon" => get_emoticon}
+  return_value
 end
 
 def get_japanese_emoticon
