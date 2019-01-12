4d79bdfdc02c2e27634353768e93e719baa6fa89
Luke Granger <grangerl330@gmail.com>
2019-01-12 02:19:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ def get_japanese_emoticon(file_path, emoticon)
     if emoticon == english
       translation = japanese
     else
-      puts "So"
+      puts "Sorry, that emoticon was not found"
     end
   end 
   translation
