c6a44d269bfe6be17f94ba9d20344538600967ec
Luke Granger <grangerl330@gmail.com>
2019-01-12 02:28:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,8 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   library = load_library(file_path)
-  library["get_emoticon"][emoticon] 
+  library["get_emoticon"][emoticon] || "Sorry, that emoticon was not found"
+end 
 
 def get_english_meaning
   # code goes here
