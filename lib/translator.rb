00ea55ffcad282a63c2e22e5518ebd947bd6e337
Luke Granger <grangerl330@gmail.com>
2019-01-12 02:33:28 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,6 +22,7 @@ def get_japanese_emoticon(file_path, emoticon)
   end 
 end 
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path, emoticon)
+  library = load_library(file_path)
+  
 end
